d7131d4d7b9e60adca3769281f52d82af01da021
bilalkarakollu <karakollubilal@gmail.com>
2022-04-19 15:59:32 +0300

    project created

---
+++ src/store/slices/dataSlice.js
@@ -0,0 +1,45 @@
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
+import axios from "axios";
+
+const initialState = {
+    data: [],
+    country: '',
+    loading: true,
+    error: ''
+}
+
+export const fetchAsyncData = createAsyncThunk('dataSlice/fetchAsyncData', async (value = 0, { getState }) => {
+    const { country } = getState().data;
+    try {
+        let fetchURL = await country ? `https://covid19.mathdro.id/api/countries/${country}` : 'https://covid19.mathdro.id/api';
+        const { data } = await axios.get(fetchURL);
+        return data;
+    } catch (err) {
+        return err.message;
+    }
+});
+
+export const dataSlice = createSlice({
+    name: 'data',
+    initialState,
+    reducers: {
+        changeCountry: (state, action) => {
+            state.country = action.payload
+        }
+    },
+    [fetchAsyncData.fulfilled]: (state, action) => {
+        state.data = action.payload;
+        state.loading = false;
+    },
+    [fetchAsyncData.pending]: (state) => {
+        state.loading = true;
+    },
+    [fetchAsyncData.rejected]: (state, action) => {
+        state.error = action.payload;
+        state.loading = false;
+    }
+});
+
+export const { changeCountry } = dataSlice.actions;
+
+export default dataSlice.reducer;
